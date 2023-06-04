e2c0e6fa80b60ed68bccf117b441231b2e62b4a1
sleepy-bear <jew8969@gmail.com>
2023-06-04 23:54:22 +0900

    apigateway 추가

---
+++ src/main/java/com/example/apigatewaysvc/ApigatewaySvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewaysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaySvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaySvcApplication.class, args);
+	}
+
+}
